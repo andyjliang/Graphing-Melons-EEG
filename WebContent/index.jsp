39f0684689493083a60b46d141e88eae8dde95dc
andyjliang <andy.j.liang@gmail.com>
2015-04-18 18:29:07 -0700

    added clientside directories. Still need to pass focusObj object into knockout js. Sooo closeeee

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -8,10 +8,29 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	    <title>Melon</title>
+	
+	    <!-- Bootstrap Core CSS -->
+	    <link href="css/bootstrap.min.css" rel="stylesheet">
+	
+	    <!-- Custom CSS -->
+	    <link href="css/main.css" rel="stylesheet">
+	
+	    <!-- Custom Fonts -->
+	    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+	    <link href='http://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+	    <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+	    <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+		
+		<script language="JavaScript" src="js/jquery-2.1.1.min.js"></script>
+		<script language="JavaScript" src="js/knockout-3.2.0.js"></script>
+		<script language="JavaScript" src="js/main.js"></script>
 </head>
-<body>
+<body id="page-top" class="index">
 	<script type="text/javascript">
 	var focusObj = []; 
 <%
@@ -22,10 +41,41 @@
 <%
 	}
 %>
-	var k;
+	var debugging;
 	</script>
-	<p>
-	Words:
-	</p>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <a class="navbar-brand page-scroll" href="#page-top">Melon</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Header -->
+    <header>
+        <div id= "headerContainer" class="container">
+            <div class="intro-text">
+                <div class="intro-lead-in cover">Think Brightly!</p></div>
+                <div class="intro-heading cover">See Focus</p></div>
+                <button id="graphButton" data-bind="click: toggleBackground" class="page-scroll btn btn-xl cover">view</button>
+            
+
+			    
+        
+            </div>
+        </div>
+    </header>
 </body>
 </html>
---
+++ WebContent/js/main.js
@@ -0,0 +1,120 @@
+/**
+ * @author anliang
+ */
+$(function(){
+	
+	function ButtonViewModel(){
+		var self = this;
+		self.toggleBackground = function(){
+			$('header').toggleClass('clicked');
+			$('header').css('background-image', 'none');
+			$('.cover').each(function(){
+				$(this).toggleClass('hidden');
+			})
+			$('.content').removeClass('hidden');
+			$('.content').removeClass('running').delay(10).queue(function(next){
+				$(this).addClass('running');
+		        next();
+		    });
+		    return false;
+		}
+	}
+	
+	function GraphViewModel(){
+		var self = this;
+		self.ballShiftL = 0;
+		self.pulseShiftL = 0;
+		self.lineShiftL = 0;
+		self.stepWidth = 100;
+		self.points = ko.observableArray([
+			{y: 10, tooltip: 'one'},
+			{y: 110, tooltip: 'two'},
+			{y: 210, tooltip: 'three'},
+			{y: 210, tooltip: 'four'},
+			{y: 110, tooltip: 'five'},
+			{y: 10, tooltip: 'six'},
+			{y: 190, tooltip: 'seven'},
+			{y: 20, tooltip: 'eight'},
+			{y: 80, tooltip: 'nine'},
+			{y: 140, tooltip: 'ten'},
+			{y: 30, tooltip: 'eleven'},
+			{y: 95, tooltip: 'twelve'}
+		]);
+		
+		self.angle = function(yInd){
+			if (yInd +1 == this.points().length) return 0;
+			var h = this.points()[yInd+1].y - this.points()[yInd].y;
+			self.setKeyFrameMoveLength(h, yInd);
+			var r = -(Math.atan(h/self.stepWidth)* (180/Math.PI));
+			return 'rotate(' + r + 'deg)';
+		}
+		
+		self.setKeyFrameMoveLength = function(h, yInd){
+			var hyp = Math.sqrt(h*h+10000);
+			var rule = "@-webkit-keyframes move" + yInd + " {0% { width:0px;} 100% { width:" + hyp + "px; box-shadow:0px 0px 5px 1px rgba(0,198,255,0.5); }}"
+			var rule2 = "@-moz-keyframes move" + yInd + " {0% { width:0px;} 100% { width:" + hyp + "px; box-shadow:0px 0px 5px 1px rgba(0,198,255,0.5); }}"
+			if(navigator.userAgent.indexOf("MSIE") > -1) document.styleSheets[0].insertRule(rule, 0);
+			if(navigator.userAgent.indexOf("Mozilla") > -1) document.styleSheets[0].insertRule(rule2, 0);
+		}
+		
+		self.leftShiftInc = function(el){
+			var returnEl; 
+			switch(el.className){
+				case 'ball':
+					self.ballShiftL += self.stepWidth;
+					returnEl = self.ballShiftL - 5;
+					break;
+				case 'pulse':
+					self.pulseShiftL += self.stepWidth;
+					returnEl = self.pulseShiftL -7;
+					break;
+				case 'line':
+					self.lineShiftL += self.stepWidth;
+					returnEl = self.lineShiftL;
+					break;	
+				default: 
+					break;
+			}
+			return returnEl + 'px';
+		}
+		
+		self.bottomShiftInc = function(el, bottomShift){
+			var returnEl; 
+			switch(el.className){
+				case 'ball':
+					returnEl = bottomShift - 5;
+					break;
+				case 'pulse':
+					returnEl = bottomShift -7;
+					break;
+				case 'line':
+					returnEl = bottomShift;
+					break;	
+				default: 
+					break;
+			}
+			return returnEl + 'px';
+		}
+		
+		// self.someFunc = function(){
+			// document.styleSheets[0].insertRule('@-moz-keyframes {\
+			    // 0%   {width:0px;}\
+			    // 100% {width:150px; box-shadow:0px 0px 5px 1px rgba(0,198,255,0.5);}\
+			// }',1);
+		// }
+
+		self.myFunc = function(){
+			// document.styleSheets[0].cssRules[9].appendRule("0% { width:0px;}");
+			// document.styleSheets[0].cssRules[9].appendRule("100% { width:100px; box-shadow:0px 0px 5px 1px rgba(0,198,255,0.5);}");
+			// document.styleSheets[0].insertRule(".line {transform: rotate(-40deg);}",3);
+			var k = 0; 
+		}
+	}
+	
+	
+	$(document).ready(function() {
+		ko.applyBindings(new ButtonViewModel(), document.getElementById('graphButton'));
+		ko.applyBindings(new GraphViewModel(), document.getElementById('graph'));
+	});
+	
+});
